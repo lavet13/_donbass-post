deb4c36341cf2169deb3f46d9662afebf548bda6
lavet13 <lavet13@mail.ru>
2025-10-23 03:19:01 +0300

    fix: theme is back

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -1,4 +1,6 @@
+import { useTheme } from "@/hooks/use-theme";
 import { cn } from "@/lib/utils";
+import { Theme } from "@radix-ui/themes";
 import type { ComponentProps, FC } from "react";
 import { Drawer as DrawerPrimitive } from "vaul";
 
@@ -29,22 +31,31 @@ const DrawerContent: FC<ComponentProps<typeof DrawerPrimitive.Content>> = ({
   className,
   ...props
 }) => {
+  const { resolvedTheme } = useTheme();
+
   return (
     <DrawerPrimitive.Portal>
       <DrawerPrimitive.Overlay className="bg-blackA-6 fixed inset-0" />
-      <DrawerPrimitive.Content
-        data-slot="drawer-content"
-        className={cn(
-          "text-gray-12 bg-gray-2 border-grayA-6 border",
-          "outline-none group/drawer-content fixed z-50 flex h-auto flex-col",
-          "data-[vaul-drawer-direction=top]:inset-x-0 data-[vaul-drawer-direction=top]:top-0 data-[vaul-drawer-direction=top]:mb-24 data-[vaul-drawer-direction=top]:max-h-[80vh] data-[vaul-drawer-direction=top]:rounded-b-lg data-[vaul-drawer-direction=top]:border-b",
-          "data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:mt-24 data-[vaul-drawer-direction=bottom]:max-h-[80vh] data-[vaul-drawer-direction=bottom]:rounded-t-lg data-[vaul-drawer-direction=bottom]:border-t",
-          "data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:right-0 data-[vaul-drawer-direction=right]:w-3/4 data-[vaul-drawer-direction=right]:border-l data-[vaul-drawer-direction=right]:sm:max-w-sm",
-          "data-[vaul-drawer-direction=left]:inset-y-0 data-[vaul-drawer-direction=left]:left-0 data-[vaul-drawer-direction=left]:w-3/4 data-[vaul-drawer-direction=left]:border-r data-[vaul-drawer-direction=left]:sm:max-w-sm",
-          className,
-        )}
-        {...props}
-      />
+      <Theme
+        appearance={resolvedTheme}
+        accentColor="red"
+        grayColor="gray"
+        panelBackground="translucent"
+      >
+        <DrawerPrimitive.Content
+          data-slot="drawer-content"
+          className={cn(
+            "text-gray-12 bg-gray-2 border-grayA-6 border",
+            "group/drawer-content fixed z-50 flex h-auto min-h-0 flex-col outline-none",
+            "data-[vaul-drawer-direction=top]:inset-x-0 data-[vaul-drawer-direction=top]:top-0 data-[vaul-drawer-direction=top]:mb-24 data-[vaul-drawer-direction=top]:max-h-[80vh] data-[vaul-drawer-direction=top]:rounded-b-lg data-[vaul-drawer-direction=top]:border-b",
+            "data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:mt-24 data-[vaul-drawer-direction=bottom]:max-h-[80vh] data-[vaul-drawer-direction=bottom]:rounded-t-lg data-[vaul-drawer-direction=bottom]:border-t",
+            "data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:right-0 data-[vaul-drawer-direction=right]:w-3/4 data-[vaul-drawer-direction=right]:border-l data-[vaul-drawer-direction=right]:sm:max-w-sm",
+            "data-[vaul-drawer-direction=left]:inset-y-0 data-[vaul-drawer-direction=left]:left-0 data-[vaul-drawer-direction=left]:w-3/4 data-[vaul-drawer-direction=left]:border-r data-[vaul-drawer-direction=left]:sm:max-w-sm",
+            className,
+          )}
+          {...props}
+        />
+      </Theme>
     </DrawerPrimitive.Portal>
   );
 };
@@ -57,7 +68,7 @@ const DrawerHandle: FC<ComponentProps<typeof DrawerPrimitive.Handle>> = ({
     <DrawerPrimitive.Handle
       data-slot="drawer-handle"
       className={cn(
-        "data-[vaul-handle]:bg-grayA-6! mx-auto mt-4 mb-1 hidden h-2 w-[100px] shrink-0 cursor-grab rounded-full active:cursor-grabbing sm:mb-0 lg:data-[vaul-handle]:hidden!",
+        "data-[vaul-handle]:bg-grayA-6! mx-auto mt-4 mb-1 hidden h-2 w-[100px] shrink-0 cursor-grab rounded-full group-data-[vaul-drawer-direction=bottom]/drawer-content:block active:cursor-grabbing sm:mb-0",
         className,
       )}
       {...props}
