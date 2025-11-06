b269987622082d7bd198d0fe4221fa27bec7f8c4
lavet13 <lavet13@mail.ru>
2025-11-06 11:41:30 +0300

    specific base url for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/user",
+  base:
+    process.env.GITHUB_ACTIONS === "true" ? "/_donbass-post/user/" : "/user",
   plugins: [
     tailwindcss(),
     tanstackRouter({
