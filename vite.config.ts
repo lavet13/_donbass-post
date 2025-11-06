a2fff72e03e94df32b2cc934ff3911b8ca8c4a38
lavet13 <lavet13@mail.ru>
2025-11-06 11:43:25 +0300

    not quite right url

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base:
-    process.env.GITHUB_ACTIONS === "true" ? "/_donbass-post/user/" : "/user",
+    process.env.GITHUB_ACTIONS === "true" ? "/_donbass-post" : "/user",
   plugins: [
     tailwindcss(),
     tanstackRouter({
