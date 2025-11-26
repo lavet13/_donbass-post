9537954115c3473e7ddcb4081bf18f4c8e12974d
lavet13 <lavet13@mail.ru>
2025-11-27 01:38:47 +0300

    redirect hack for github pages

---
+++ prettier.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
